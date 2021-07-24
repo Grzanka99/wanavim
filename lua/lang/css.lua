2fb7d2a57b6e632282d17dab77c88a2c4c7bb5b9
Cezary Grzanka <cezarygrzanka@gmail.com>
2021-07-24 02:52:27 +0200

    Enable snippetSupport in css

--- lua/lang/css.lua
+++ lua/lang/css.lua
@@ -1,5 +1,7 @@
 -- npm install -g vscode-css-languageserver-bin
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 require "lspconfig".cssls.setup {
-    cmd = {"css-languageserver", "--stdio"}
+    capabilities = capabilities
 }
