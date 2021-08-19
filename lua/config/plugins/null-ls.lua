e22114e5087832888c28b8d3fb6999619a1493e3
Cezary Grzanka <cezarygrzanka@gmail.com>
2021-08-19 12:41:11 +0200

    Swtiched from eslint to eslint_d bcs works much batter

--- lua/config/plugins/null-ls.lua
+++ lua/config/plugins/null-ls.lua
@@ -4,8 +4,7 @@ local sources = {
 	null_ls.builtins.formatting.prettier,
 	null_ls.builtins.formatting.stylua,
 	null_ls.builtins.formatting.clang_format,
-	null_ls.builtins.diagnostics.eslint,
-	null_ls.builtins.formatting.editorconfig,
+	null_ls.builtins.diagnostics.eslint_d,
 	null_ls.builtins.formatting.whitespace,
 }
 
