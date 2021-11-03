c50d3d53619cc7c8e22468d6b1d9b32bcaa23696
Cezary Grzanka <Cezary_Grzanka@epam.com>
2021-11-03 21:30:40 +0100

    Add context string for more filetypes

--- lua/config/plugins/nvim-comment.lua
+++ lua/config/plugins/nvim-comment.lua
@@ -1,7 +1,18 @@
+local fileTypes = { "vue", "typescript", "typescriptreact", "javascript", "javascriptreact" }
+
+local function contains(table, val)
+	for i = 1, #table do
+		if table[i] == val then
+			return true
+		end
+	end
+	return false
+end
+
 require("nvim_comment").setup({
 	create_mappings = false,
 	hook = function()
-		if vim.api.nvim_buf_get_option(0, "filetype") == "vue" then
+		if contains(fileTypes, vim.api.nvim_buf_get_option(0, "filetype")) then
 			require("ts_context_commentstring.internal").update_commentstring()
 		end
 	end,
