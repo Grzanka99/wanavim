d189a75b50c031d860da2a9ec4e110389659a326
Cezary Grzanka <cezarygrzanka@gmail.com>
2021-11-09 10:04:54 +0100

    Remove commented built-ins from null-ls config

--- lua/config/plugins/null-ls.lua
+++ lua/config/plugins/null-ls.lua
@@ -1,15 +1,6 @@
 local null_ls = require("null-ls")
 
-local sources = {
-  -- null_ls.builtins.formatting.prettier,
-  -- null_ls.builtins.formatting.stylua,
-  -- null_ls.builtins.formatting.clang_format,
-  -- null_ls.builtins.formatting.eslint_d,
-  -- null_ls.builtins.formatting.whitespace,
-  -- null_ls.builtins.diagnostics.eslint_d,
-  -- null_ls.builtins.diagnostics.luacheck,
-  -- null_ls.builtins.diagnostics.shellcheck,
-}
+local sources = {}
 
 for _, formatter in pairs(__.formatters) do
 	if formatter.enabled then
