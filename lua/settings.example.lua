c64fb8b2adb9acc86cb1c94a4798eccf3796e5bc
Cezary Grzanka <cezarygrzanka@gmail.com>
2021-11-09 10:08:05 +0100

    Add some docs to settings.example.lua

--- lua/settings.example.lua
+++ lua/settings.example.lua
@@ -1,3 +1,7 @@
+-- copy this file into lua/settings.lua
+
+-- in this array, you decide which of hand-configured lsp servers
+-- are enabled (lua/lang)
 __.lsp = {
 	{ name = "lspinstall", enabled = true },
 	{ name = "css", enabled = true },
@@ -9,6 +13,7 @@ __.lsp = {
 	{ name = "dart", enabled = false },
 }
 
+-- Enable/disable null-ls built-ins formatters
 __.formatters = {
 	{ name = "prettier", enabled = true },
 	{ name = "stylua", enabled = true },
@@ -17,23 +22,28 @@ __.formatters = {
 	{ name = "trim_whitespace", enabled = true },
 }
 
+-- Enable/disable null-ls built-ins diagnostics
 __.diagnostics = {
 	{ name = "eslint_d", enabled = true },
 	{ name = "shellcheck", enabled = true },
 	{ name = "luacheck", enabled = false },
 }
 
+-- Additional colorschemes
 __.installedColorschemes = {
 	{ url = "christianchiarulli/nvcode-color-schemes.vim", enabled = true },
 	{ url = "marko-cerovac/material.nvim", enabled = true },
 }
 
+-- set colorscheme
 __.colorscheme = "material"
 vim.g.material_style = "deep ocean"
 
+-- set guifont
 __.guifont = {
 	name = "FiraCode Nerd Font",
 	size = 12,
 }
 
+-- Set lang
 __.language = "en_US.utf8"
