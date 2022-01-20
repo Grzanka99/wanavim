e84042f7d2546f1fc740f10bc158b14ef111bb5b
Cezary Grzanka <cezarygrzanka@gmail.com>
2022-01-20 22:47:12 +0100

    Add updated volar config

--- lua/lang/volar.lua
+++ lua/lang/volar.lua
@@ -1,24 +1,26 @@
-local util = require("lspconfig.util")
+-- local util = require("lspconfig.util")
+local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+capabilities.textDocument.completion.completionItem.snippetSupport = true
 
-local function get_typescript_server_path(root_dir)
-	local project_root = util.find_node_modules_ancestor(root_dir)
+-- local function get_typescript_server_path(root_dir)
+-- 	local project_root = util.find_node_modules_ancestor(root_dir)
 
-	local local_tsserverlib = project_root ~= nil
-		and util.path.join(project_root, "node_modules", "typescript", "lib", "tsserverlibrary.js")
-	local global_tsserverlib = "/home/[yourusernamehere]/.npm/lib/node_modules/typescript/lib/tsserverlibrary.js"
+-- local local_tsserverlib = project_root ~= nil
+-- 	and util.path.join(project_root, "node_modules", "typescript", "lib", "tsserverlibrary.js")
+-- local global_tsserverlib = "/usr/lib/node_modules/typescript/lib/tsserverlibrary.js"
 
-	if local_tsserverlib and util.path.exists(local_tsserverlib) then
-		return local_tsserverlib
-	else
-		return global_tsserverlib
-	end
-end
+-- 	if local_tsserverlib and util.path.exists(local_tsserverlib) then
+-- 		return local_tsserverlib
+-- 	else
+-- 		return global_tsserverlib
+-- 	end
+-- end
 
 require("lspconfig").volar.setup({
-	filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue", "json" },
-	config = {
-		on_new_config = function(new_config, new_root_dir)
-			new_config.init_options.typescript.serverPath = get_typescript_server_path(new_root_dir)
-		end,
-	},
+	-- filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue", "json" },
+	-- config = {
+	-- 	on_new_config = function(new_config, new_root_dir)
+	-- 		new_config.init_options.typescript.serverPath = get_typescript_server_path(new_root_dir)
+	-- 	end,
+	-- },
 })
