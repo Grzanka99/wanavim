869c7211d0141010c207c7e4e7e99996c92fcd15
Cezary Grzanka <cezarygrzanka@gmail.com>
2023-06-21 17:02:08 +0200

    coq_nvim kinda ok

---
+++ lua/config/plugins/lsp/coq.lua
@@ -0,0 +1,9 @@
+vim.g.coq_settings = {
+	auto_start = true,
+	keymap = {
+		jump_to_mark = "null",
+	},
+	match = {
+		fuzzy_cutoff = 0.8,
+	},
+}
--- lua/config/plugins/lsp/init.lua
+++ lua/config/plugins/lsp/init.lua
@@ -1,16 +1,8 @@
 require("config.plugins.lsp.lsp")
--- require("config.plugins.lsp.cmp")
+require("config.plugins.lsp.coq")
 require("config.plugins.lsp.mason")
 require("config.plugins.lsp.null-ls")
 require("config.plugins.lsp.lspsaga")
 require("config.plugins.lsp.project")
 
-require("lsp_signature").setup()
 require("fidget").setup({})
-
-vim.g.coq_settings = {
-	auto_start = true,
-	keymap = {
-		jump_to_mark = "null",
-	},
-}
