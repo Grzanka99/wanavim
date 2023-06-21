f8ca7c36529ae44af2fb3e2056742142f7ac9425
Cezary Grzanka <cezarygrzanka@gmail.com>
2023-06-21 17:31:20 +0200

    coq_nvim more stuff, trying make it good

--- lua/config/plugins/lsp/coq.lua
+++ lua/config/plugins/lsp/coq.lua
@@ -1,9 +1,22 @@
 vim.g.coq_settings = {
 	auto_start = true,
 	keymap = {
+		pre_select = true,
 		jump_to_mark = "null",
 	},
 	match = {
 		fuzzy_cutoff = 0.8,
+	},
+	clients = {
+		snippets = {
+			always_on_top = false,
+			enabled = false,
+		},
+		lsp = {
+			resolve_timeout = 0.06,
+		},
+	},
+	completion = {
+		skip_after = {},
 	},
 }
