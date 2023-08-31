67c769e96a2f5c7c3cd4a04614757d55240e5730
Cezary Grzanka <cezary_grzanka@epam.com>
2023-08-31 16:02:24 +0200

    Small improvement to neotree

--- lua/config/neotree.lua
+++ lua/config/neotree.lua
@@ -1,19 +1,25 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-    keys = {
-      { "<M-space>", "<cmd>Neotree toggle<cr>", desc = "NeoTree" },
-    },
-    config = function()
-      require("neo-tree").setup({
-        window = {
-          position = "right",
-          width = 40,
-        }
-      })
-    end,
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-    }
+	"nvim-neo-tree/neo-tree.nvim",
+	keys = {
+		{ "<M-space>", "<cmd>Neotree toggle<cr>", desc = "NeoTree" },
+	},
+	config = function()
+		require("neo-tree").setup({
+			filesystem = {
+				follow_current_file = {
+					enabled = true,
+					leave_dirs_open = true,
+				},
+			},
+			window = {
+				position = "right",
+				width = 40,
+			},
+		})
+	end,
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim",
+	},
 }
